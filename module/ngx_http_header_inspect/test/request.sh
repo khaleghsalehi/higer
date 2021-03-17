5d0c3153e6ecd8a5eb81db3810de85261b0678aa
Kahelgh Salehi <khaleghsalehi@yahoo.com>
2021-03-18 04:25:49 +0700

    added test case.

--- module/ngx_http_header_inspect/test/request.sh
+++ module/ngx_http_header_inspect/test/request.sh
@@ -7,7 +7,14 @@
 # online aes encryption/ decryption page: https://www.devglan.com/online-tools/aes-encryption-decryption
 #
 
+
+# must return 200
 curl -v --location --request GET 'http://127.0.0.1' \
 --header 'xtoken: 8ED8FECD70BC3F95C5F43C33AAAB477513981253585391E50AE6599FEEF692A0146A038654095380F7D9FCB6FA901F7A' \
 --header 'token-version: 00000111'
 
+
+# return 400, bad request
+curl -v --location --request GET 'http://127.0.0.1' \
+--header 'xtoken: 8ED8FECD70BC3F95C5F43C33AAAB477513981253585391E50AE6599FEEF692A0146A038654095380F7D9FCB6FA901F7A' \
+--header 'token-version: 00000000'
